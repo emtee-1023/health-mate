c582e34aae8012ba493b629e14d3e1da77cd2bd5
emtee-1023 <marktalamson@gmail.com>
2024-10-21 22:46:19 +0300

    remote db connected

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,7 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'health_mate');
-$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+define('DB_SERVER', 'sql7.freesqldatabase.com');
+define('DB_USERNAME', 'sql7739447');
+define('DB_PASSWORD', 'jD7SgRJ2Ce');
+define('DB_DATABASE', 'sql7739447');
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 $pdo = $conn;
-?>
