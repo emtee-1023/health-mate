f87d933c58c5ba974e53778e9f52139ffe93b7c7
emtee-1023 <marktalamson@gmail.com>
2024-10-29 13:09:04 +0300

    db pharma

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'sql7.freesqldatabase.com');
-define('DB_USERNAME', 'sql7739447');
-define('DB_PASSWORD', 'jD7SgRJ2Ce');
-define('DB_DATABASE', 'sql7739447');
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'inlaqkew_root');
+define('DB_PASSWORD', '@IH&pS%l6v+Z');
+define('DB_DATABASE', 'inlaqkew_health_mate');
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, 5522);
 $pdo = $conn;
