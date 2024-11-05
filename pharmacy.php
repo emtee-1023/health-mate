85b9740f8f3d8eb81143d52187924e62690b005b
Benson <benson.gitonga@strathmore.edu>
2024-11-05 13:33:26 +0300

    ss

--- pharmacy.php
+++ pharmacy.php
@@ -85,7 +85,7 @@
         }
 
         .card {
-            background-color: white;
+            background-color: yellow;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
