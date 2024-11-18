3307b5d6b34218e103dbd951f3fc97bb1e1fceec
emtee-1023 <marktalamson@gmail.com>
2024-11-19 01:54:16 +0300

    staging

--- doctors/includes/sidebar.php
+++ doctors/includes/sidebar.php
@@ -2,19 +2,18 @@
  <aside class="main-sidebar sidebar-light-primary elevation-4">
    <!-- Brand Logo -->
    <a href="dashboard.php" class="brand-link">
-     <img src="../assets/bootstrap-logo.png" alt="Logo" class="brand-image " style="opacity: .8">
-     <span class="brand-text font-weight-light">ADMIN PANEL</span>
-   </a>
+     <img src="../img/logo.jpg" alt="Logo" class="brand-image" style="opacity: .8; width: 200px; max-height: 40px;">
+   </a> <br>
 
    <!-- Sidebar -->
    <div class="sidebar">
      <!-- Sidebar user panel (optional) -->
      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
        <div class="image">
-         <img src="../assets/bootsrap-logo.png" class="img-circle elevation-2" alt="User Image">
+         <img src="../uploads/<?= $_SESSION['pfp'] ?>" class="img-circle elevation-2" alt="User Image">
        </div>
        <div class="info">
-         <a href="#" class="d-block">Health Mate</a>
+         <a href="#" class="d-block">Dr. <?= $_SESSION['fname'] . ' ' . $_SESSION['lname'] ?> <br> <span>Doctor</span></a>
        </div>
      </div>
 
@@ -84,9 +83,8 @@
      </nav>
      <!-- /.sidebar-menu -->
      <div class="bottom">
-       <a href="processes.php?logout" class="btn btn-info text-white btn-block">
+       <a href="processes.php?logout" class="btn btn-primary text-white btn-block">
          <i class="nav-icon fas fa-sign-out-alt"></i>
-
          Logout
        </a>
      </div>
